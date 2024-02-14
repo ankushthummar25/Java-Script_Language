bca695307fe597dee216e01eef3329b47ce794b4
ankush25 <ankushthummar25@gmail.com>
2024-02-14 10:46:30 +0530

    14-2-2024

--- Set Method/set.js
+++ Set Method/set.js
@@ -1,34 +1,36 @@
 // Set Method
 
-let data = new Set([10,20,30,40,50, "Hello"]);
+// let data = new Set([10,20,30,40,50, "Hello"]);
 
-let txt = new Set();  // declare
-data.add("Skill");
-console.log(data);
-
-
-txt.add('a');
-txt.add('b');
-txt.add('c');
-txt.add('d');
-console.log(txt);
+// let txt = new Set();  // declare
+// data.add("Skill");
+// console.log(data);
 
 
-data.delete(30);
+// txt.add('a');
+// txt.add('b');
+// txt.add('c');
+// txt.add('d');
+// console.log(txt);
 
-data.has(30);
 
-console.log(data.has(80));
-console.log(data.entries());
+// data.delete(30);
 
-data.clear()
-console.log(data);
-console.log(data.size);
-console.log(data.values());
+// data.has(30);
+
+// console.log(data.has(80));
+// console.log(data.entries());
+
+// data.clear()
+// console.log(data);
+// console.log(data.size);
+// console.log(data.values());
 
 
 let txt = "";
-data.forEach(Function(val){
+data.forEach(Function(val)
+{
     txt += val + "\n";
-})
+}
+)
 console.log(txt);
