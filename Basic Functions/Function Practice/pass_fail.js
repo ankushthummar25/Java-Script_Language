f5b491c0ebdac6ae364d4a107bc5e4d2a11dd9a7
ankush25 <ankushthummar25@gmail.com>
2024-02-19 08:16:38 +0530

    17-02-2024

---
+++ Basic Functions/Function Practice/pass_fail.js
@@ -0,0 +1,20 @@
+let A;
+
+function add(A)
+{
+    if(A>33)
+    {
+        console.log("A is Pass")
+    }else{
+        console.log("A is Fail")
+    }
+    return a;
+}
+console.log(add(68));
+
+
+/*
+
+A is Pass
+
+*/
