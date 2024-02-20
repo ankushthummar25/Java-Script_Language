626043d8f06bf80a496277271a914bd89e197aac
ankush25 <ankushthummar25@gmail.com>
2024-02-20 11:17:43 +0530

    20-02-2024

---
+++ String/string1.js
@@ -0,0 +1,96 @@
+let a = "Hello 'math' World";           // Double quotes
+let b = 'Hello "box" World';            // Single quotes
+
+console.log(a);
+console.log(b);
+
+
+
+
+
+//  //                   String Length
+
+// let data = 'Javascript learning';
+// console.log(data.length);                              //  => 19
+// console.log(data[8]);                                  //  => p
+
+
+
+
+
+//  //                      String split() 
+
+// let data = 'Javascript learning';
+// console.log(data.split(''));                          //  => (19) ['J', 'a', 'v', 'a', 's', 'c', 'r',
+//                                                       //           'i', 'p', 't', ' ', 'l', 'e', 'a', 
+//                                                       //           'r', 'n', 'i', 'n', 'g']
+
+
+
+
+
+
+// //  //                  String toUpperCase()
+// let data = 'Javascript learning';
+// console.log(data.toUpperCase());                      // => JAVASCRIPT LEARNING
+
+
+
+
+
+//  //                      String toLowerCase()
+
+// let data = 'Javascript learning';
+// console.log(data.toLowerCase());                     // => javascript learning
+
+
+
+
+
+//  //                    String trim()
+
+// let data = '       Javascript learning       ';
+// console.log(data.length);                           //  => 33
+// console.log(data.trim());                           //  => Javascript learning
+
+
+
+
+
+//  //                     String trimStart()
+
+// let data = '       Javascript learning       ';
+// console.log(data.trimEnd());                       // =>        Javascript learning
+// console.log(data.trimEnd().length);                // => 26
+
+
+
+
+
+//  //                      String trimEnd()
+
+// let data = '       Javascript learning       ';
+// console.log(data.trimStart());                      // => Javascript learning
+// console.log(data.trimStart().length);               // => 26
+
+
+
+
+
+
+//  //                       String padStart() 
+
+// let txt = '12532'
+// console.log(txt.padStart(10 , 'X'));               // => XXXXX12532
+
+
+
+
+
+
+//   //                     String padEnd()
+
+// let txt = '12532'
+// console.log(txt.padEnd(10 , 'X'));                 // => 12532XXXXX
+
+
