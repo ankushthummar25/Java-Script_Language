6685db5a93a48cbf52117771edca2f0214799d8f
ankush25 <ankushthummar25@gmail.com>
2024-02-27 11:21:22 +0530

    27-02-2024

--- Example/Function/ex5.js
+++ Example/Function/ex5.js
@@ -4,3 +4,25 @@
        checks whether a number is perfect.
 
 */
+
+
+function check (num){
+    let sum = 0;
+
+    for(let i=1; i<= num/2 ; i++)
+    {
+        if(num%i == 0)
+        {
+            sum = sum + i;
+        }
+    }
+
+    if(num == sum){
+        console.log("It is a Perfect Number");
+    }else{
+        console.log("It is Not Perfect Number");
+    }
+}
+
+console.log(check(8));                             // => It is Not Perfect Number
+console.log(check(6));                             // => It is a Perfect Number
