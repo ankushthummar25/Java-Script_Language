ce8c3b86a6ee684a613b514961c06953569ddd4f
ankush25 <ankushthummar25@gmail.com>
2024-02-28 08:24:11 +0530

    28-02-2024

--- Example/Function/ex4.js
+++ Example/Function/ex4.js
@@ -16,8 +16,9 @@ function Longest_Word(str)
     
     for (var i = 0; i < word.length; i++) 
     {
-     
+        word[i] = word[i].Math.max.apply(null,i);
     }
+    return word[i].join("");
 }
   console.log(Longest_Word('Web Development Tutorial'));
   
