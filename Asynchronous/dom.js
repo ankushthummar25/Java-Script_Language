4a815197df4aeb999fad684323ac8bd0a8c3542a
ankush25 <ankushthummar25@gmail.com>
2024-03-07 09:55:04 +0530

    06-03-2024

---
+++ Asynchronous/dom.js
@@ -0,0 +1,6 @@
+function findElement() {
+    const element = document.getElementById("id1");
+    element.innerHTML = "element found!.......";
+    element.style.color="blu";
+    document.getElementById("id2").innerHTML = "hello world......!" 
+}
