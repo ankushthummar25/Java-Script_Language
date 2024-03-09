a7e33645f4548a9aa5d64ac36f2b4104870a934b
ankush25 <ankushthummar25@gmail.com>
2024-03-09 09:24:07 +0530

    23-02-2024

--- Date Object/set_Me_date.js
+++ Date Object/set_Me_date.js
@@ -1,5 +1,65 @@
 let date = new Date();
 
-date.setHours(9);
-console.log(date);
+// date.setMilliseconds(100000);
+// console.log(date);                                 // Sat Mar 09 2024 09:06:21 GMT+0530 (India Standard Time)
+
+// date.setUTCMilliseconds(10000000);
+// console.log(date);                                 // Sat Mar 09 2024 12:03:03 GMT+0530 (India Standard Time)
+
+
+
+
+// date.setSeconds(1000);
+// console.log(date);                                 // Sat Mar 09 2024 09:21:40 GMT+0530 (India Standard Time)
+
+// date.setUTCSeconds(1000);
+// console.log(date);                                 // Sat Mar 09 2024 09:33:40 GMT+0530 (India Standard Time)
+
+
+
+
+// date.setMinutes(20);
+// console.log(date);                                 // Sat Mar 09 2024 09:20:19 GMT+0530 (India Standard Time)
+
+
+
+
+// date.setHours(12);
+// console.log(date);                                 // Sat Mar 09 2024 12:07:28 GMT+0530 (India Standard Time)
+
+// date.setUTCHours(12);
+// console.log(date);                                 // Sat Mar 09 2024 17:34:40 GMT+0530 (India Standard Time)
+
+
+
+
+// date.setDate(25);
+// console.log(date);                                 // Mon Mar 25 2024 09:08:26 GMT+0530 (India Standard Time)
+
+// date.setUTCDate(25);
+// console.log(date);                                 // Mon Mar 25 2024 09:14:59 GMT+0530 (India Standard Time)
+
+
+
+
+// date.setMonth(1);
+// console.log(date);                                 // Fri Feb 09 2024 09:09:16 GMT+0530 (India Standard Time)
+
+// date.setUTCMonth(1);
+// console.log(date);                                 // Fri Feb 09 2024 09:19:45 GMT+0530 (India Standard Time)
+
+
+
+
+// date.setFullYear(2002);
+// console.log(date);                                 // Sat Mar 09 2002 09:10:16 GMT+0530 (India Standard Time)
+
+// date.setUTCFullYear(2030);
+// console.log(date);                                 // Sat Feb 09 2030 09:20:20 GMT+0530 (India Standard Time)
+
+
+
+
+// date.setTime(10000000);
+// console.log(date);                                 // Thu Jan 01 1970 08:16:40 GMT+0530 (India Standard Time)
 
